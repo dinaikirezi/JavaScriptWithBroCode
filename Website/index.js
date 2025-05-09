0102e5168445167b37a172875c2a8f84c01b6ec1
Dina Ikirezi <ikirezidina@gmail.com>
2025-05-09 21:53:27 +0200

    third commit

--- Website/index.js
+++ Website/index.js
@@ -4,3 +4,4 @@
 //window.alert(`This is an alert!`);
 //window.alert(`I really like Pizza!`);
 document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Pizza`;
