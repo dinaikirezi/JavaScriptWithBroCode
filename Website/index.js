9e91532a66c5e226a05c2cf8b04d7741e4ae0d81
Dina Ikirezi <ikirezidina@gmail.com>
2025-05-09 21:51:18 +0200

    second commit

--- Website/index.js
+++ Website/index.js
@@ -2,4 +2,5 @@
 //console.log(`I like Pizza`);
 
 //window.alert(`This is an alert!`);
-// window.alert(`I really like Pizza!`);
+//window.alert(`I really like Pizza!`);
+document.getElementById("myH1").textContent = `Hello`;
