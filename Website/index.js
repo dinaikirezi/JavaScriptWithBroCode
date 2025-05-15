a803a83ad503e0ef26dc9699f419d02d56efe480
Dina Ikirezi <ikirezidina@gmail.com>
2025-05-15 13:13:51 +0200

    Reversing a sentence

--- Website/index.js
+++ Website/index.js
@@ -4,8 +4,10 @@
 //window.alert(`This is an alert!`);
 //window.alert(`I really like Pizza!`);
 
-/*document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like Pizza`;*/
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `I like Pizza`;
 let squareArray = (arr) => arr.map(x => x * x);
 console.log (squareArray([1,4,6,2,7]));
 
+let word = "I am a developer";
+let wordArray = word.split(" ").reverse().join(" ");
